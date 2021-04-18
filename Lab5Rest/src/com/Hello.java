476dc99233d1e4d3a96454e41106dba1771f480c
pramodC135 <pramodmadhushan4@gmail.com>
2021-04-18 23:43:47 +0530

    HelloName

--- Lab5Rest/src/com/Hello.java
+++ Lab5Rest/src/com/Hello.java
@@ -2,6 +2,7 @@ package com;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces; 
 import javax.ws.rs.core.MediaType;
 
@@ -15,5 +16,14 @@ public class Hello {
 	 { 
 			return "Hello world."; 
 	 } 
+	
+	@GET
+	@Path("/{name}") 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String helloName(@PathParam("name") String name) 
+	{ 
+	return "Hello " + name; 
+	}
+
 	
 }
